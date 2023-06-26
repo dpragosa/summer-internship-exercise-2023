862f3b054ba39826695c88964ead528f3bc280e5
dpragosa <d.pragosa@campus.fct.unl.pt>
2023-06-26 15:41:01 +0100

    snail cicle added

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -6,7 +6,8 @@ import java.util.concurrent.Future;
  * Created by aamado on 05-05-2023.
  */
 class SnailShellPattern implements ISnailShellPattern {
-
+  private ExecutorService executor
+    = Executors.newSingleThreadExecutor();
   /**
    * Method to get snailshell pattern
    * 
@@ -14,6 +15,49 @@ class SnailShellPattern implements ISnailShellPattern {
    * @return order array of values thar represent a snail shell pattern
    */
   public Future<int[]> getSnailShell(int[][] matrix) {
-    throw new RuntimeException("Not Implemented Yet");
+
+    return executor.submit(() -> {
+
+      int n =  matrix.length;
+      int up = 0;
+      int down = n-1;
+      int left = 0;
+      int right = n-1;
+      int i;
+      int j = 0;
+      int aux = 0;
+      int [] result = new int[n*n];
+
+    while(j < n*n){
+
+      for(i = 0; i < n-up; i++){
+        result[j] = matrix[up][i+aux];
+        j++;
+      }
+      up++;
+
+      for(i = 0; i < right-aux; i++){
+        result[j] = matrix[i+up][right];
+        j++;
+      }
+      right--;
+
+      for(i = n-1; i-aux > aux; i--){
+        result[j] = matrix[down][i-up];
+        j++;
+      }
+      down--;
+
+      for(i = n-1; i-aux > aux; i--){
+        result[j] = matrix[i-up][left];
+        j++;
+      }
+      left++;
+
+      aux++;
+    }
+
+      return result;
+    });
   };
 }
