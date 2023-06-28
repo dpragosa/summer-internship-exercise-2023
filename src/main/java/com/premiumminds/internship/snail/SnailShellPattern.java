e4e2017e220d90578979c36020ab97c276756792
dpragosa <d.pragosa@campus.fct.unl.pt>
2023-06-28 13:49:39 +0100

    snail shell updated and tests added

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -3,6 +3,7 @@ package com.premiumminds.internship.snail;
 import java.util.concurrent.Future;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.Arrays;
 
 /**
  * Created by aamado on 05-05-2023.
@@ -20,51 +21,56 @@ class SnailShellPattern implements ISnailShellPattern {
 
     return executor.submit(() -> {
 
+      int [] empty = new int[0];
+
+      if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+        return empty;
+      }
+
       int n =  matrix.length;
-      int up = 0;
-      int down = n-1;
-      int left = 0;
-      int right = n-1;
+      int up = 0;                   //upper line of matrix
+      int down = n-1;               //lower line of matrix
+      int left = 0;                 //left line of matrix
+      int right = n-1;              //right line of matrix
       int i;
       int j = 0;
-      int aux = 0;
-      int [] result = new int[n*n];
+      int nlaps = 0;                //counts the number of laps/cycles
+      int [] result = new int[n*n]; //array with the resulted snail path
 
-    while(true){
+      while(true){
 
-      if(j == n*n){ break;}
-      for(i = 0; i < n-up; i++){
-        result[j] = matrix[up][i+aux];
-        j++;
+        if(j == n*n){ break;}               //breaks the cycle when the snail path is completed
+        for(i = 0; left+i <= right; i++){   //cycle to retrieve the upper row
+          result[j] = matrix[up][i+nlaps];
+          j++;
+        }
+        up++;
+
+        if(j == n*n){ break;}               //breaks the cycle when the snail path is completed
+        for(i = 0; up+i <= down; i++){      //cycle to retrieve the right column
+          result[j] = matrix[i+up][right];
+          j++;
+        }
+        right--;
+
+        if(j == n*n){ break;}               //breaks the cycle when the snail path is completed
+        for(i = n-1; i-nlaps > nlaps; i--){ //cycle to retrieve the lower row
+          result[j] = matrix[down][i-up];
+          j++;
+        }
+        down--;
+
+        if(j == n*n){ break;}                 //breaks the cycle when the snail path is completed
+        for(i = n-1; i-nlaps > nlaps+1; i--){ //cycle to retrieve the left column
+          result[j] = matrix[i-up][left];
+          j++;
+        }
+        left++;
+
+        nlaps++;
       }
-      up++;
 
-
-      if(j == n*n){ break;}
-      for(i = 0; i < right-aux; i++){
-        result[j] = matrix[i+up][right];
-        j++;
-      }
-      right--;
-
-      if(j == n*n){ break;}
-      for(i = n-1; i-aux > aux; i--){
-        result[j] = matrix[down][i-up];
-        j++;
-      }
-      down--;
-
-      if(j == n*n){ break;}
-      for(i = n-1; i-aux > aux; i--){
-        result[j] = matrix[i-up][left];
-        j++;
-      }
-      left++;
-
-      aux++;
-    }
-
-      return result;
+    return result;
     });
   };
 }
--- src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
+++ src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
@@ -25,6 +25,49 @@ public class SnailShellPatternTest {
   public SnailShellPatternTest() {
   };
 
+
+  /**
+   *Test for empty matrix
+   */
+  @Test
+  public void ScreenLockinPatternTestFirst0Length2Test()
+          throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = new int[0][0];
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { };
+    assertArrayEquals(result, expected);
+  }
+
+  /**
+   *Test for matrix 1x1
+   */
+  @Test
+  public void ScreenLockinPatternTestFirst1Length2Test()
+          throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { { 1 } };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1 };
+    assertArrayEquals(result, expected);
+  }
+
+  /**
+   *Test for matrix 2x2
+   */
+  @Test
+  public void ScreenLockinPatternTestFirst2Length2Test()
+          throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { { 1, 2 }, { 4, 3 } };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1, 2, 3, 4 };
+    assertArrayEquals(result, expected);
+  }
+
+  /**
+   *Test for matrix 3x3
+   */
   @Test
   public void ScreenLockinPatternTestFirst3Length2Test()
       throws InterruptedException, ExecutionException, TimeoutException {
@@ -34,4 +77,57 @@ public class SnailShellPatternTest {
     int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     assertArrayEquals(result, expected);
   }
+
+  /**
+   *Test for matrix 5x5
+   */
+  @Test
+  public void ScreenLockinPatternTestFirst5Length2Test()
+          throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { { 1, 2, 3, 4, 5 },
+                       { 16, 17, 18, 19, 6 },
+                       { 15, 24, 25, 20, 7 },
+                       { 14, 23, 22, 21, 8 },
+                       { 13, 12, 11, 10, 9 } };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+    assertArrayEquals(result, expected);
+  }
+
+  /**
+   *Test for matrix 8x8
+   */
+  @Test
+  public void ScreenLockinPatternTestFirst8Length2Test()
+          throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { { 1, 2, 3, 4, 5, 6, 7, 8 },
+                      { 28, 29, 30, 31, 32, 33, 34, 9 },
+                      { 27, 48, 49, 50, 51, 52, 35, 10 },
+                      { 26, 47, 60, 61, 62, 53, 36, 11 },
+                      { 25, 46, 59, 64, 63, 54, 37, 12 },
+                      { 24, 45, 58, 57, 56, 55, 38, 13 },
+                      { 23, 44, 43, 42, 41, 40, 39, 14 },
+                      { 22, 21, 20, 19, 18, 17, 16, 15 }};
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+                      27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+                      51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64};
+    assertArrayEquals(result, expected);
+  }
+
+  /**
+   *Test for matrix 3x3 with negative numbers
+   */
+  @Test
+  public void ScreenLockinPatternTestFirstNeg3Length2Test()
+          throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { { -1, -2, -3 }, { -8, -9, -4 }, { -7, -6, -5 } };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { -1, -2, -3, -4, -5, -6, -7, -8, -9 };
+    assertArrayEquals(result, expected);
+  }
+
 }
